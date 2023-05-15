641982d1950e8d8c2db24f7cf915ef2afeb150a0
Ricardo <v@prismacloudsecuritydemo.com>
2023-05-15 14:22:06 +0530

    updated ecr tf

--- iac/ecr.tf
+++ iac/ecr.tf
@@ -1,15 +1,18 @@
 resource aws_ecr_repository "repository" {
-  name                 = "ebilling-ecr-${var.ebillingid}"
+  name                 = "ebilling-ecr-3retrfdf"
   image_tag_mutability = "MUTABLE"
 
-  tags = merge({
-    git_commit           = "e6d83b21346fe85d4fe28b16c0b2f1e0662eb1d7"
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+  tags = {
+    git_commit           = "N/A"
     git_file             = "iac/ecr.tf"
-    git_last_modified_at = "2023-04-27 12:47:51"
-    git_last_modified_by = "nsadkfjh@lsajdf.com"
-    git_modifiers        = "ricardo7364/ricardo"
-    git_org              = "bridgecrewio"
-    git_repo             = "terragoat"
-    yor_trace            = "7a3ec657-fa54-4aa2-8467-5d08d6c90bc2"
-    })
+    git_last_modified_at = "2023-05-15 08:48:24"
+    git_last_modified_by = "v@prismacloudsecuritydemo.com"
+    git_modifiers        = "v"
+    git_org              = "ricardo7364"
+    git_repo             = "backend-app"
+    yor_trace            = "3251a9e3-d124-4486-b3c3-77803b27dba0"
+  }
 }
